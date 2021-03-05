702a17c8f1b2e0003cc2c13f6b56909555c99dda
Laura Ines <laura27german@hotmail.com>
2021-03-05 14:00:12 -0400

    practica4 terminada

---
+++ practica4/3.0.js
@@ -0,0 +1,17 @@
+function perimetro (a) 
+{
+    area= a*4;
+    return area;
+
+}
+function superficie(b)
+{
+    m= b*b;
+    return m;
+
+
+}
+perimetro (9);
+superficie(4);
+
+document.writeln("El perimetro es " +area + " y la superficie es "+ m);
